9e9cb434a7f146cfad21813f80d99b28c1b8efd6
= <=>
2022-11-12 17:41:06 +0100

    MiniProjet : correction des cercles qui bougent

--- MiniProjet/main.c
+++ MiniProjet/main.c
@@ -25,19 +25,17 @@ int random_number(int min_num, int max_num) {
 
 // def DrawGreeny
 void DrawGreeny(int centreGreenyX, int centreGreenyY) {
-    float rayonGreeny = 10;
+    float rayonGreeny = 25;
     DrawCircle(centreGreenyX, centreGreenyY, rayonGreeny, GREEN);
 }
 
 // def DrawObjects
-void DrawObjects(int nbreObjets) {
-    for (int i=0; i<nbreObjets; i++) {
-        int centreX = random_number(0,800);
-        int centreY = random_number(0,450);
-        float rayon = random_number(5,60);
-        int numCouleur = random_number(0,6);
-        Color tabCouleurs[] = {YELLOW, BLUE, ORANGE, PINK, PURPLE, RED};
-        Color couleur = tabCouleurs [numCouleur];
+void DrawObjects(int centresX[], int centresY[], float rayons[], Color couleurs[]) {
+    for (int i=0; i<2000; i++) {
+        int centreX = centresX[i];
+        int centreY = centresY[i];
+        float rayon = rayons[i];
+        Color couleur = couleurs[i];
         DrawCircle(centreX, centreY, rayon, couleur);
     }
 }
@@ -48,9 +46,9 @@ void DrawObjects(int nbreObjets) {
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
-int main ()
-{
-    // Initialization
+int main () {
+    
+    // INITIALISATION
     //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
     const int screenHeight = 450;
@@ -62,20 +60,39 @@ int main ()
 
     SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
 
+
+    // INITIALISATION DE GREENY : constantes de position de Greeny
     const int centreGreenyX = random_number(0,800);
     const int centreGreenyY = random_number(0,450);
+
+
+    // INITIALISATION DES OBJETS : constantes de position et caractéristiques des objets aléatoires
+    const int nbrObjets = 2000;
+    int centresCerclesX[nbrObjets];
+    int centresCerclesY[nbrObjets];
+    float rayonsCercles[nbrObjets];
+    Color couleursCercles[nbrObjets];
+    Color tabCouleurs[] = {YELLOW, BLUE, ORANGE, PINK, PURPLE, RED};
+    for (int i=0; i<nbrObjets; i++) {
+        centresCerclesX[i] = random_number(-2400,2400);
+        centresCerclesY[i] = random_number(-1200,1200);
+        rayonsCercles[i] = random_number(10,100);
+        int numCouleur = random_number(0,5);
+        couleursCercles[i] = tabCouleurs [numCouleur];
+    }
     //--------------------------------------------------------------------------------------
 
-    // constantes de position de Greeny
+
+
+
 
     // Main game loop
-    while (!WindowShouldClose())        // Detect window close button or ESC key
-    {
+    while (!WindowShouldClose()) {         // Detect window close button or ESC key
+
         // Update
         //----------------------------------------------------------------------------------
         // Translate based on mouse right click
-        if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT))
-        {
+        if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
             Vector2 delta = GetMouseDelta();
             delta = Vector2Scale(delta, -1.0f/camera.zoom);
 
@@ -84,8 +101,7 @@ int main ()
 
         // Zoom based on mouse wheel
         float wheel = GetMouseWheelMove();
-        if (wheel != 0)
-        {
+        if (wheel != 0) {
             // Get the world point that is under the mouse
             Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera);
             
@@ -126,18 +142,16 @@ int main ()
                     DrawGrid(100, 50);
                 rlPopMatrix();
 
-                // Draw a reference circle
-                DrawCircle(100, 100, 50, YELLOW);
-
                 // Draw plenty of random (not green) objects
-                DrawObjects(500);
+                DrawObjects(centresCerclesX, centresCerclesY, rayonsCercles, couleursCercles);
 
                 // Draw Greeny
-                DrawGreeny(centreGreenyX, centreGreenyY);
+                // DrawGreeny(centreGreenyX, centreGreenyY);
+                DrawGreeny(0, 0);
                 
             EndMode2D();
 
-            DrawText("Où est Greeny ? Mouse right button drag to move, mouse wheel to zoom, mouse left button to click on Greeny", 10, 10, 20, WHITE);
+            DrawText("Où est Greeny ? \nMouse right button drag to move, mouse wheel to zoom. \nMouse left button to click on Greeny.", 10, 10, 20, WHITE);
         
         EndDrawing();
         //----------------------------------------------------------------------------------
